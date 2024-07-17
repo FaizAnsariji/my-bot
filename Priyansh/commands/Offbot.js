a8ba53fe81bb827eabad33d4262c6717ba86d447
FaizAnsariji <164743041+FaizAnsariji@users.noreply.github.com>
2024-07-17 19:25:15 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100029323637418", "100029323637418"];
+    const permission = ["61552656562989", "100086169630526"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("आपको इस कमांड को उपयोग करने की परमिशन नही है।\nये कमांड सिर्फ मेरे बॉस फैज अंसारी के लिए है। 🙂🙂✌️", event.threadID, event.messageID);
   api.sendMessage(`┏━━━━━┓\n     फैज-अंसारी                   ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nOK BYE ALL FRIENDS\n${global.config.BOTNAME} BOT AB OFF HO RAHA HAI\nBYE BYE LADKIYO MERA INTZAAR KRNA 😒✌️\n\n❁ ═════════ ❃AZAAN-ANSARI❃ ═════════ ❁`,event.threadID, () =>process.exit(0))
 }
